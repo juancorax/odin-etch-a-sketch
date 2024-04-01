d05bd582f173887692502c8d87f8a2ba06251b6c
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-04-01 16:25:06 -0300

    feat: squares change color when mouse enters them
    Randomize the squares’ RGB values with each interaction.
    
    Implement a progressive darkening effect where
    each interaction darkens the square by 10%.

--- script.js
+++ script.js
@@ -7,6 +7,22 @@ for (let i = 1; i <= 16; i++) {
   for (let j = 1; j <= 16; j++) {
     const div = document.createElement("div");
 
+    div.addEventListener("mouseover", () => {
+      // gets a random integer between two values, inclusive
+      function getRandomIntInclusive(min, max) {
+        const minCeiled = Math.ceil(min);
+        const maxFloored = Math.floor(max);
+        return Math.floor(
+          Math.random() * (maxFloored - minCeiled + 1) + minCeiled,
+        );
+      }
+
+      div.style.backgroundColor = `rgb(${getRandomIntInclusive(0, 255)}, ${getRandomIntInclusive(0, 255)}, ${getRandomIntInclusive(0, 255)})`;
+
+      div.style.opacity =
+        parseFloat(window.getComputedStyle(div).opacity) - 0.1;
+    });
+
     rowContainer.appendChild(div);
   }
 
