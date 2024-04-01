dc0411180a2b8e37e73212d9233c3744b668af95
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-04-01 18:01:20 -0300

    feat: add a button that changes the grid size

--- script.js
+++ script.js
@@ -1,30 +1,66 @@
 const columnContainer = document.querySelector("#column-container");
 
-for (let i = 1; i <= 16; i++) {
-  const rowContainer = document.createElement("div");
-  rowContainer.classList.add("row-container");
+function createGrid(numberOfSquaresPerSide) {
+  numberOfSquaresPerSide = Number(numberOfSquaresPerSide);
 
-  for (let j = 1; j <= 16; j++) {
-    const div = document.createElement("div");
+  for (let i = 1; i <= numberOfSquaresPerSide; i++) {
+    const rowContainer = document.createElement("div");
+    rowContainer.classList.add("row-container");
 
-    div.addEventListener("mouseover", () => {
-      // gets a random integer between two values, inclusive
-      function getRandomIntInclusive(min, max) {
-        const minCeiled = Math.ceil(min);
-        const maxFloored = Math.floor(max);
-        return Math.floor(
-          Math.random() * (maxFloored - minCeiled + 1) + minCeiled,
-        );
-      }
+    for (let j = 1; j <= numberOfSquaresPerSide; j++) {
+      const div = document.createElement("div");
 
-      div.style.backgroundColor = `rgb(${getRandomIntInclusive(0, 255)}, ${getRandomIntInclusive(0, 255)}, ${getRandomIntInclusive(0, 255)})`;
+      div.addEventListener("mouseover", () => {
+        // gets a random integer between two values, inclusive
+        function getRandomIntInclusive(min, max) {
+          const minCeiled = Math.ceil(min);
+          const maxFloored = Math.floor(max);
+          return Math.floor(
+            Math.random() * (maxFloored - minCeiled + 1) + minCeiled,
+          );
+        }
 
-      div.style.opacity =
-        parseFloat(window.getComputedStyle(div).opacity) - 0.1;
-    });
+        div.style.backgroundColor = `rgb(${getRandomIntInclusive(0, 255)}, ${getRandomIntInclusive(0, 255)}, ${getRandomIntInclusive(0, 255)})`;
 
-    rowContainer.appendChild(div);
+        div.style.opacity =
+          parseFloat(window.getComputedStyle(div).opacity) - 0.1;
+      });
+
+      rowContainer.appendChild(div);
+    }
+
+    columnContainer.appendChild(rowContainer);
+  }
+}
+
+createGrid(16);
+
+const gridSizeButton = document.querySelector("#grid-size-button");
+
+gridSizeButton.addEventListener("click", () => {
+  let userInput;
+
+  for (let j = 0; j < 1; j++) {
+    userInput = prompt(
+      "Enter the number of squares per side for the new grid:\n(max. 100)",
+      "16",
+    );
+
+    if (
+      userInput === "" ||
+      userInput === null ||
+      isNaN(userInput) ||
+      userInput < 0 ||
+      userInput > 100
+    ) {
+      j--;
+      continue;
+    }
   }
 
-  columnContainer.appendChild(rowContainer);
-}
+  while (columnContainer.hasChildNodes()) {
+    columnContainer.removeChild(columnContainer.lastChild);
+  }
+
+  createGrid(userInput);
+});
